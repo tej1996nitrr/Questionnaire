ce55a97ab7ebe2ee458018e33ab2fec50c412e1b
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-25 23:19:15 +0530

    retrieve update delete answers as author

--- questionaire/questions/api/urls.py
+++ questionaire/questions/api/urls.py
@@ -2,14 +2,16 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import QuestionViewset as qv
-from .views import AnswerCreateAPIView,AnswerListAPIView
+from .views import AnswerCreateAPIView,AnswerListAPIView, AnswerRUDAPIView
 
 router = DefaultRouter()
 router.register(r"", qv)
 
 urlpatterns = [
     path("questions/", include(router.urls)),
-    path("questions/<slug:slug>/answer/",AnswerCreateAPIView.as_view(), name="create-answer" ),
+    path("questions/<slug:slug>/answer/",AnswerCreateAPIView.as_view(), name="answer-create" ),
     path("questions/<slug:slug>/answers/",AnswerListAPIView.as_view(), name="answer-list" ),
+    path("answers/<int:pk>",AnswerRUDAPIView.as_view(), name="answer-detail" ),
+
 
 ] 
--- questionaire/questions/api/views.py
+++ questionaire/questions/api/views.py
@@ -35,4 +35,10 @@ class AnswerListAPIView(generics.ListAPIView):
     def get_queryset(self):
         kwarg_slug = self.kwargs.get("slug")
         return Answer.objects.filter(question__slug=kwarg_slug).order_by("-created_at")
-        
+
+class AnswerRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated,IsAuthorOrReadOnly]
+
+
