0eaca7956a022390c5c951c3fb97cb04cea9894c
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-25 20:39:18 +0530

    answers serializer

--- questionaire/questions/api/views.py
+++ questionaire/questions/api/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
-from questions.api.serializers import QuestionSerializer
+from rest_framework import viewsets, generics
+from rest_framework.exceptions import ValidationError
+from questions.api.serializers import QuestionSerializer,AnswerSerializer
 from questions.models import Question 
 from rest_framework.permissions import IsAuthenticated
 from questions.api.permissions import IsAuthorOrReadOnly
@@ -14,3 +15,16 @@ class QuestionViewset(viewsets.ModelViewSet):
 
     def perform_create(self, serializer): #to add author field automatically    
         serializer.save(author=self.request.user)
+
+class AnswerViewset(generics.CreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+   
+    def perform_create(self, serializer): #to add author field automatically  
+        request_user = self.request.user
+        kwarg_slug = self.kwargs.get("slug")
+        question = get_object_or_404(Answer, slug=k)
+        if question.answers.filter(author=request_user).exists():
+            raise ValidationError("You have already answered this question.") 
+        serializer.save(author=self.request.user, question=question)
