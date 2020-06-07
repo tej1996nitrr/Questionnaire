f5f4856bb47a21461a6679b592d41ca23dd8d881
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-06-07 16:06:39 +0530

    changes

--- questionaire/frontend/vue.config.js
+++ questionaire/frontend/vue.config.js
@@ -1,5 +1,8 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
+
+
+
 module.exports = {
     // on Windows you might want to set publicPath: "http://127.0.0.1:8080/" 
     publicPath: "http://0.0.0.0:8080/", 
