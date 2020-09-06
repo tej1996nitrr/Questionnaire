fe7e311d7057f511784eb23ea27618454ea52ed3
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-06 18:17:46 +0530

    small chng

--- questionaire/frontend/src/App.vue
+++ questionaire/frontend/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <NavbarComponent/>
+    <HomeComponent/>
+     
     <router-view/>
   </div>
 </template>
 
 <script>
 import NavbarComponent from './components/Navbar.vue'
+import HomeComponent from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    
   }
 }
 </script>
--- questionaire/frontend/src/components/Home.vue
+++ questionaire/frontend/src/components/Home.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="home">
+        <div class="container">
+
         <img alt="Vue Logo" src="/assets/logo.png"/>
         <h1>Home Page</h1>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    name:"HomeComponent"
+}
+</script>
