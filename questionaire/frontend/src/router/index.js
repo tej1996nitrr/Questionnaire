cbf989e8dda24105557d1008ee8b8e1cf16b1e82
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-12 17:25:34 +0530

    testing route

--- questionaire/frontend/src/router/index.js
+++ questionaire/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Example from "../views/Example.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/example",
+    name: "Example",
+    component: Example
   },
   {
     path: "/about",
